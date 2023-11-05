98a3984e26d14879ead88fa21417d3aaaee6c267
vinceTheProgrammer <vincetheprogrammer@gmail.com>
2023-11-05 04:45:02 -0600

    add toggle vanilla subtitles keybind save config whenever toggle verbose subtitles keybind is pressed

--- src/main/java/io/github/vincetheprogrammer/verbosesubtitles/event/KeyInputHandler.java
+++ src/main/java/io/github/vincetheprogrammer/verbosesubtitles/event/KeyInputHandler.java
@@ -10,13 +10,19 @@ import org.lwjgl.glfw.GLFW;
 public class KeyInputHandler {
     public static final String KEY_CATEGORY_SOUND_INFO = "Verbose Subtitles Mod";
     public static final String KEY_TOGGLE_SOUND_INFO = "Toggle Verbose Subtitles";
+    public static final String KEY_TOGGLE_VANILLA_SUBTITLES = "Toggle Subtitles";
 
     public static KeyBinding toggleVerboseSubtitlesKey;
+    public static KeyBinding toggleVanillaSubtitlesKey;
 
     public static void registerKeyInputs() {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if(toggleVerboseSubtitlesKey.wasPressed()) {
                 VerboseSubtitlesConfig.INSTANCE.enabled = !VerboseSubtitlesConfig.INSTANCE.enabled;
+                VerboseSubtitlesConfig.saveConfig();
+            }
+            if(toggleVanillaSubtitlesKey.wasPressed()) {
+                client.options.getShowSubtitles().setValue(!client.options.getShowSubtitles().getValue());
             }
         });
     }
@@ -28,7 +34,12 @@ public class KeyInputHandler {
                 GLFW.GLFW_KEY_G,
                 KEY_CATEGORY_SOUND_INFO
         ));
-
+        toggleVanillaSubtitlesKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                KEY_TOGGLE_VANILLA_SUBTITLES,
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_V,
+                KEY_CATEGORY_SOUND_INFO
+        ));
         registerKeyInputs();
     }
 }
--- src/main/java/io/github/vincetheprogrammer/verbosesubtitles/integration/VerboseSubtitlesModMenuIntegration.java
+++ src/main/java/io/github/vincetheprogrammer/verbosesubtitles/integration/VerboseSubtitlesModMenuIntegration.java
@@ -4,14 +4,8 @@ import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import io.github.vincetheprogrammer.verbosesubtitles.config.VerboseSubtitlesConfig;
 import me.shedaniel.autoconfig.AutoConfig;
-import me.shedaniel.clothconfig2.api.AbstractConfigListEntry;
-import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.text.Text;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Environment(EnvType.CLIENT)
 public class VerboseSubtitlesModMenuIntegration implements ModMenuApi {
